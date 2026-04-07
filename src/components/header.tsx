f29e831eafc639e9a02b27fbaaacad4941fbb1a3
duduSilvaCosta <eduardo.costa@ufra.edu.br>
2026-04-07 10:46:33 -0300

    link no h1

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,21 +1,24 @@
 import Image from "next/image";
 import styles from "@/style/header.module.css";
 import img from "@/assets/512x512.png";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logoBorder}>
-        <Image
-          src={img}
-          alt="Logo"
-          width={50}
-          height={50}
-          className={styles.logo}
-          priority
-        />
-      </div>
-      <h1>Guia do Pará</h1>
+    <header>
+      <Link href="/" className={styles.header}>
+        <div className={styles.logoBorder}>
+          <Image
+            src={img}
+            alt="Logo"
+            width={50}
+            height={50}
+            className={styles.logo}
+            priority
+          />
+        </div>
+        <h1>Guia do Pará</h1>
+      </Link>
     </header>
   );
 }
